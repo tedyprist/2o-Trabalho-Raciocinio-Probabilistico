a869cc912c3da9e559e9cb53cb5135075d46b2d9
prist <100169478+tedyprist@users.noreply.github.com>
2025-05-20 14:49:41 -0400

    Add files via upload

---
+++ questao2.pl
@@ -0,0 +1,33 @@
+
+% Probabilidades básicas
+0.5::str(dry); 0.3::str(wet); 0.2::str(snow_covered).
+0.2::flw.
+
+0.95::b.
+0.95::k.
+
+% Dínamo deslizante (R) condicionado
+0.9::r :- str(snow_covered), flw.
+0.6::r :- str(snow_covered), \+flw.
+0.7::r :- str(wet), flw.
+0.3::r :- str(wet), \+flw.
+0.4::r :- str(dry), flw.
+0.05::r :- str(dry), \+flw.
+
+% Voltagem (V) depende de R
+0.9::v :- r.
+0.2::v :- \+r.
+
+% Luz ligada (Li) depende de V, B, K
+0.99::li :- v, b, k.
+0.9::li :- v, b, \+k.
+0.9::li :- v, \+b, k.
+0.7::li :- v, \+b, \+k.
+0.2::li :- \+v, b, k.
+0.1::li :- \+v, b, \+k.
+0.1::li :- \+v, \+b, k.
+0.01::li :- \+v, \+b, \+k.
+
+% Consulta
+query(v).
+evidence(str(snow_covered)).
